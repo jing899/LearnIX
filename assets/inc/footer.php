9c571d8ce3bdebab6d0512996da17c9052515083
jhealy277 <45016059+jhealy277@users.noreply.github.com>
2019-03-22 13:29:07 -0400

    Add files via upload
    starting css, footer and header files

---
+++ assets/inc/footer.php
@@ -0,0 +1,9 @@
+<!-- LEARNIX -->
+<!-- Rachel Bogdany -->
+<!-- Jon Healy -->
+<!-- Lowell Pence -->
+<!-- Shoujing Wu -->
+	
+	<footer>This is the footer</footer>
+</body>
+</html>
---
+++ assets/inc/header.php
@@ -0,0 +1,15 @@
+<!-- LEARNIX -->
+<!-- Rachel Bogdany -->
+<!-- Jon Healy -->
+<!-- Lowell Pence -->
+<!-- Shoujing Wu -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title><?php echo $page;?></title>
+	<link rel="stylesheet" type="text/css" href="assets/css/styles.css" />
+	<script type="text/javascript" src="assets/script/pizza.js"></script>
+</head>
+<body>
+	<p>This is the start of the body</p>
