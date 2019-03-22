aaac73cd7b5de146a3af961b2cca0189d966bf96
Rachael <rxb9323@rit.edu>
2019-03-22 18:25:36 -0400

    Changed srcs

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -8,8 +8,8 @@
 <html lang="en">
 <head>
 	<title><?php echo $page;?></title>
-	<link rel="stylesheet" type="text/css" href="assets/css/styles.css" />
-	<script type="text/javascript" src="assets/script/pizza.js"></script>
+	<link rel="stylesheet" type="text/css" href="<?php echo $path;?>assets/css/styles.css"/>
+	<script type="text/javascript" src="<?php echo $path;?>assets/script/main.js"></script>
 </head>
 <body>
 	<p>This is the start of the body</p>
