2e158fdf574c5f378bd67b8a07cf349ed4ce9a42
Rachael <rxb9323@rit.edu>
2019-03-30 14:34:04 -0400

    Create tutorial1.php

---
+++ tutorial1.php
@@ -0,0 +1,18 @@
+<!-- LEARNIX 
+	
+Filename:	tutorial1.php
+Authors: 	Rachael Bogdany
+			Jon Healy
+			Lowell Pence
+			Shoujing Wu 
+-->
+<?php
+	//include the header
+	$path = './';
+	$page = 'Navigation';
+	include $path.'assets/inc/header.php';
+?>
+<?php
+	//include the footer
+	include $path.'assets/inc/footer.php';
+?>
