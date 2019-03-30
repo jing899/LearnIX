7de4c2adcea76a02512b60bc07cd251d532e9456
Rachael <rxb9323@rit.edu>
2019-03-30 14:43:00 -0400

    Update header.php

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -19,11 +19,46 @@ Authors: 	Rachael Bogdany
 		<img src="<?php echo $path;?>assets/img/logo.jpg" alt="Logo"/>
 		<nav id="nav"> 
 			<ul>
-				<li><a href="">Home</a></li>
-				<li><a>Novice</a></li>
-				<li><a>Intermediate</a></li>
-				<li><a>Advanced</a></li>
-				<li><a>About</a></li>
+				<li><a href="">Home</a>
+					<ul>
+						<li><a>Unix Architecture</a></li>
+						<li><a>Kernel v.s. Shell</a></li>
+					</ul>
+				</li>
+				<li><a>Novice</a>
+					<ul>
+						<li><a>Navigation</a></li>
+						<li><a>File Creation</a></li>
+						<li><a>Permissions</a></li>
+						<li><a>Command Line Programs</a></li>
+						<li><a>Basics</a></li>
+					</ul>
+				</li>
+				<li><a>Intermediate</a>
+					<ul>
+						<li><a>System Information</a></li>
+						<li><a>Pip/Yum</a></li>
+						<li><a>Processes</a></li>
+						<li><a>Root Level</a></li>
+						<li><a>SSH</a></li>
+						<li><a>Commands</a></li>
+					</ul>
+				</li>
+				<li><a>Advanced</a>
+					<ul>
+						<li><a>Advanced Programs</a></li>
+						<li><a>Regular Expressions</a></li>
+						<li><a>Users</a></li>
+						<li><a>Advanced Commands</a></li>
+						<li><a></a></li>
+					</ul>
+				</li>
+				<li><a>About</a>
+					<ul>
+						<li><a>Content Citations</a></li>
+						<li><a>Competitive Analysis Citations</a></li>
+					</ul>
+				</li>
 			</ul>
 		</nav>
 	</header>
