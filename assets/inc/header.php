506a7f915390dcdd23ad53b98dd863f1c4776acb
Lowell Pence <lxp3901@rit.edu>
2019-03-31 21:56:18 -0400

    Added navigation styling
    Styling for the navigation, and the updated HTML for the header file. Will work on the hamburger menu functionality during the week.

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -1,65 +1,65 @@
-<!-- LEARNIX 
-	
+<!-- LEARNIX
+
 Filename:	header.php
 Authors: 	Rachael Bogdany
 			Jon Healy
 			Lowell Pence
-			Shoujing Wu 
+			Shoujing Wu
 -->
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<title><?php echo $page;?></title>
-    
+
 	<link rel="stylesheet" type="text/css" href="<?php echo $path;?>assets/css/styles.css"/>
-    
+
 	<script type="text/javascript" src="<?php echo $path;?>assets/script/main.js"></script>
 </head>
-    
+
 <body>
 	<header>
 		<img src="<?php echo $path;?>assets/img/logo.jpg" alt="Logo"/>
-		<nav id="nav"> 
+		<nav id="nav">
 			<ul>
 				<li><a href="">HOME</a>
-					<ul>
-						<li><a>Unix Architecture</a></li>
-						<li><a>Kernel v.s. Shell</a></li>
-					</ul>
+					<div class="dropdown-content">
+						<a>Unix Architecture</a>
+						<a>Kernel v.s. Shell</a>
+					</div>
 				</li>
-				<li><a>NOVICE</a>
-					<ul>
-						<li><a>Navigation</a></li>
-						<li><a>File Creation</a></li>
-						<li><a>Permissions</a></li>
-						<li><a>Command Line Programs</a></li>
-						<li><a>Basics</a></li>
-					</ul>
+				<li class="dropdown"><a class="dropbtn">NOVICE</a>
+					<div class="dropdown-content">
+						<a>Navigation</a>
+						<a>File Creation</a>
+						<a>Permissions</a>
+						<a>Command Line Programs</a>
+						<a>Basics</a>
+                    </div>
 				</li>
-				<li><a>INTERMEDIATE</a>
-					<ul>
-						<li><a>System Information</a></li>
-						<li><a>Pip/Yum</a></li>
-						<li><a>Processes</a></li>
-						<li><a>Root Level</a></li>
-						<li><a>SSH</a></li>
-						<li><a>Commands</a></li>
-					</ul>
+				<li class="dropdown"><a class="dropbtn">INTERMEDIATE</a>
+					<div class="dropdown-content">
+						<a>System Information</a>
+						<a>Pip/Yum</a>
+						<a>Processes</a>
+						<a>Root Level</a>
+						<a>SSH</a>
+						<a>Commands</a>
+					</div>
 				</li>
-				<li><a>ADVANCED</a>
-					<ul>
-						<li><a>Advanced Programs</a></li>
-						<li><a>Regular Expressions</a></li>
-						<li><a>Users</a></li>
-						<li><a>Advanced Commands</a></li>
-					</ul>
+				<li class="dropdown"><a class="dropbtn">ADVANCED</a>
+					<div class="dropdown-content">
+						<a>Advanced Programs</a>
+						<a>Regular Expressions</a>
+						<a>Users</a>
+						<a>Advanced Commands</a>
+					</div>
 				</li>
-				<li><a>ABOUT</a>
-					<ul>
-						<li><a>Content Citations</a></li>
-						<li><a>Competitive Analysis Citations</a></li>
-					</ul>
+				<li class="dropdown"><a class="dropbtn">ABOUT</a>
+					<div class="dropdown-content">
+						<a>Content Citations</a>
+						<a>Competitive Analysis Citations</a>
+					</div>
 				</li>
 			</ul>
 		</nav>
