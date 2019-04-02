5a06e5b27fb622c58671cb2ec8e6c81e7fa16e6f
Rachael <rxb9323@rit.edu>
2019-04-01 21:54:16 -0400

    Changed navigation

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -11,12 +11,9 @@ Shoujing Wu
 <html lang="en">
     <head>
         <title><?php echo $page;?></title>
-
         <link rel="stylesheet" type="text/css" href="<?php echo $path;?>assets/css/styles.css"/>
-
         <script type="text/javascript" src="<?php echo $path;?>assets/script/main.js"></script>
     </head>
-
     <body>
         <header>
             <nav id="nav">
@@ -30,28 +27,43 @@ Shoujing Wu
                     <li class="dropdown"><a class="dropbtn">ADVANCED</a>
                         <div class="dropdown-content">
                             <a>Advanced Programs</a>
+							<a>Advanced Programs Quiz</a>
                             <a>Regular Expressions</a>
+							<a>Regular Expressions Quiz</a>
                             <a>Users</a>
+							<a>Users Quiz</a>
                             <a>Advanced Commands</a>
+							<a>Advanced Commands Quiz</a>
                         </div>
                     </li>
                     <li class="dropdown"><a class="dropbtn">INTERMEDIATE</a>
                         <div class="dropdown-content">
                             <a>System Information</a>
+							<a>System Information Quiz</a>
                             <a>Pip/Yum</a>
+							<a>Pip/Yum Quiz</a>
                             <a>Processes</a>
+							<a>Processes Quiz</a>
                             <a>Root Level</a>
+							<a>Root Level Quiz</a>
                             <a>SSH</a>
+							<a>SSH Quiz</a>
                             <a>Commands</a>
+							<a>Commands Quiz</a>
                         </div>
                     </li>
                     <li class="dropdown"><a class="dropbtn">NOVICE</a>
                         <div class="dropdown-content">
                             <a>Navigation</a>
+							<a>Navigation Quiz</a>
                             <a>File Creation</a>
+							<a>File Creation Quiz</a>
                             <a>Permissions</a>
+							<a>Permissions Quiz</a>
                             <a>Command Line Programs</a>
+							<a>Command Line Programs Quiz</a>
                             <a>Basics</a>
+							<a>Basics Quiz</a>
                         </div>
                     </li>
                     <li><a href="">HOME</a>
