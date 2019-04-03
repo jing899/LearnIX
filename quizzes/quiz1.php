6d9ca7208991996eb963f9c70a881a5c40e73559
jhealy277 <45016059+jhealy277@users.noreply.github.com>
2019-04-02 23:42:44 -0400

    finalized quiz

--- quizzes/quiz1.php
+++ quizzes/quiz1.php
@@ -14,7 +14,7 @@ Authors: 	Rachael Bogdany
 	include $path.'assets/inc/header.php';
 ?>
 
-<form name = "quiz1" onsubmit="return validateQuiz1()" action = "#" method = "get">
+<form name = "quiz1" action="#" method = "get">
 	<h1>Novice Quiz</h1>
 	<p>Test your knowledge on unix navigation and files with the following questions</p>
 	
@@ -26,42 +26,57 @@ Authors: 	Rachael Bogdany
 		it can determine which choices were choosen correctly and incorrectly and display this to the user.
 		More can be seen on this in the validateQuiz1 function in the file assets/script/main.js
 	-->	
-	<h2>Question 1:</h2>
+	<h2 id = "q1">Question 1</h2>
 	<p>What is the correct command syntax to navigate up one level?</p>
 	<input type="radio" name ="upOne" value = "q1a1" id="q1a1"/> <label for="q1a1">cd *</label><br>
 	<input type="radio" name ="upOne" value = "q1a2" id="q1a2"/> <label for="q1a2">cd up</label><br>
+	<div class = "textBox">
+		<p>Please review the man pages for proper command syntax</p>
+	</div>	
 	<input type="radio" name ="upOne" value = "correct" id="q1a3"/> <label for="q1a3">cd ..</label><br>
 	<input type="radio" name ="upOne" value = "q1a4" id="q1a4"/> <label for="q1a4">cd !</label><br>
 	
-	<h2>Question 2</h2>
+	<h2 id = "q2">Question 2</h2>
 	<p>What is the correct command syntax to get the current working directory path?</p>
 	<input type="radio" name ="curPath" value = "correct" id="q2a1"/> <label for="q2a1">pwd</label><br>
 	<input type="radio" name ="curPath" value = "q2a2" id="q2a2"/> <label for="q2a2">current -d</label><br>
+	<div class = "textBox">
+		<p>Please review the man pages for proper command syntax</p>
+	</div>
 	<input type="radio" name ="curPath" value = "q2a3" id="q2a3"/> <label for="q2a3">list path</label><br>
 	<input type="radio" name ="curPath" value = "q2a4" id="q2a4"/> <label for="q2a4">current path</label><br>
 	
-	<h2>Question 3</h2>
+	<h2 id = "q3">Question 3</h2>
 	<p>What is the correct command syntax to create a file named testing.txt?</p>
 	<input type="radio" name ="create" value = "q3a1" id="q3a1"/> <label for="q3a1">create testing.txt</label><br>
 	<input type="radio" name ="create" value = "q3a2" id="q3a2"/> <label for="q3a2">make testing.txt</label><br>
+	<div class = "textBox">
+		<p>Please review the man pages for proper command syntax</p>
+	</div>
 	<input type="radio" name ="create" value = "q3a3" id="q3a3"/> <label for="q3a3">mkf testing.txt</label><br>
 	<input type="radio" name ="create" value = "correct" id="q3a4"/> <label for="q3a4">touch testing.txt</label><br>
 	
-	<h2 id = "qs4">Question 4</h2>
+	<h2 id = "q4">Question 4</h2>
 	<p>What is the command syntax to list all sub-directories in the current directory</p>
 	<input type="radio" name ="list" value = "q4a1" id="q4a1"/> <label for="q4a1">list dirs</label><br>
 	<input type="radio" name ="list" value = "correct" id="q4a2"/> <label for="q4a2">list -d</label><br>
+	<div class = "textBox">
+		<p>Please review the man pages for proper command syntax</p>
+	</div>
 	<input type="radio" name ="list" value = "q4a3" id="q4a3"/> <label for="q4a3">listall</label><br>
 	<input type="radio" name ="list" value = "q4a4" id="q4a4"/> <label for="q4a4">listall -d /.</label><br>
 	
-	<h2 id = "qs5">Question 5</h2>
+	<h2 id = "q5">Question 5</h2>
 	<p>What is the correct command syntax to remove a directory called users?</p>
 	<input type="radio" name ="remove" value = "q5a1" id="q5a1"/> <label for="q5a1">remove dir users</label><br>
 	<input type="radio" name ="remove" value = "q5a2" id="q5a2"/> <label for="q5a2">rm -d users</label><br>
+	<div class = "textBox">
+		<p>Please review the man pages for proper command syntax</p>
+	</div>
 	<input type="radio" name ="remove" value = "correct" id="q5a3"/> <label for="q5a3">rmdir users</label><br>
 	<input type="radio" name ="remove" value = "q5a4" id="q5a4"/> <label for="q5a4">deldir users</label><br>
 	
-	<input type="submit"  name="Submit"  value="Send"  />
+	<input type="submit"  name="Submit" onclick="validateQuiz1()"  value="Send"  />
 
 </form>
 
