52f4d455b16feb42a17bc6a530280c4c9dd4f4cd
Rachael <rxb9323@rit.edu>
2019-04-06 16:13:13 -0400

    updating paths and hrefs

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -20,10 +20,10 @@ Shoujing Wu
                 <ul>
                     <li class="dropdown"><a class="dropbtn">ABOUT</a>
                         <div class="dropdown-content">
-							<a>Unix Architecture</a>
-                            <a>Kernel v.s. Shell</a>
-                            <a>Content Citations</a>
-                            <a>Analysis Citations</a>
+							<a href="<?php echo $path;?>about/architecture.php">Unix Architecture</a>
+                            <a href="<?php echo $path;?>about/kernelshell.php">Kernel v.s. Shell</a>
+                            <a href="<?php echo $path;?>about/contentcitations.php">Content Citations</a>
+                            <a href="<?php echo $path;?>about/analysiscitations.php">Analysis Citations</a>
                         </div>
                     </li>
 					<li class="dropdown"><a class="dropbtn">QUIZZES</a>
@@ -35,47 +35,47 @@ Shoujing Wu
                     </li>
                     <li class="dropdown"><a class="dropbtn">ADVANCED</a>
                         <div class="dropdown-content">
-                            <a>Advanced Programs</a>
+                            <a href="<?php echo $path;?>advanced/advancedprograms.php">Advanced Programs</a>
 							<a>Advanced Programs Quiz</a>
-                            <a>Regular Expressions</a>
+                            <a href="<?php echo $path;?>advanced/regularexpressions.php">Regular Expressions</a>
 							<a>Regular Expressions Quiz</a>
-                            <a>Users</a>
+                            <a href="<?php echo $path;?>advanced/users.php">Users</a>
 							<a>Users Quiz</a>
-                            <a>Advanced Commands</a>
+                            <a href="<?php echo $path;?>advanced/advancedcommands.php">Advanced Commands</a>
 							<a>Advanced Commands Quiz</a>
                         </div>
                     </li>
                     <li class="dropdown"><a class="dropbtn">INTERMEDIATE</a>
                         <div class="dropdown-content">
-                            <a>System Information</a>
+                            <a href="<?php echo $path;?>intermediate/systeminformation.php">System Information</a>
 							<a>System Information Quiz</a>
-                            <a>Pip/Yum</a>
+                            <a href="<?php echo $path;?>intermediate/pipyum.php">Pip/Yum</a>
 							<a>Pip/Yum Quiz</a>
-                            <a>Processes</a>
+                            <a href="<?php echo $path;?>intermediate/processes.php">Processes</a>
 							<a>Processes Quiz</a>
-                            <a>Root Level</a>
+                            <a href="<?php echo $path;?>intermediate/rootlevel.php">Root Level</a>
 							<a>Root Level Quiz</a>
-                            <a>SSH</a>
+                            <a href="<?php echo $path;?>intermediate/ssh.php">SSH</a>
 							<a>SSH Quiz</a>
-                            <a>Commands</a>
+                            <a href="<?php echo $path;?>intermediate/commands.php">Commands</a>
 							<a>Commands Quiz</a>
                         </div>
                     </li>
                     <li class="dropdown"><a class="dropbtn">NOVICE</a>
                         <div class="dropdown-content">
-                            <a>Navigation</a>
+                            <a href="<?php echo $path;?>novice/navigation.php">Navigation</a>
 							<a>Navigation Quiz</a>
-                            <a>File Creation</a>
+                            <a href="<?php echo $path;?>novice/filecreation.php">File Creation</a>
 							<a>File Creation Quiz</a>
-                            <a>Permissions</a>
+                            <a href="<?php echo $path;?>novice/permissions.php">Permissions</a>
 							<a>Permissions Quiz</a>
-                            <a>Command Line Programs</a>
+                            <a href="<?php echo $path;?>novice/commandlineprograms.php">Command Line Programs</a>
 							<a>Command Line Programs Quiz</a>
-                            <a>Basics</a>
+                            <a href="<?php echo $path;?>novice/basics.php">Basics</a>
 							<a>Basics Quiz</a>
                         </div>
                     </li>
-                    <li><a href="">HOME</a>
+                    <li><a href="<?php echo $path;?>index.php">HOME</a>
                         <div class="dropdown-content">
                         </div>
                     </li>
