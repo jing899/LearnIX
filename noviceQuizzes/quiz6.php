b3ff094db82e342e5be928189cf33912caaa85c9
jhealy277 <45016059+jhealy277@users.noreply.github.com>
2019-04-23 15:37:23 -0400

    fixed names

--- noviceQuizzes/quiz6.php
+++ noviceQuizzes/quiz6.php
@@ -18,7 +18,7 @@ Authors: 	Rachael Bogdany
 	<form onsubmit = "return false;" name = "quiz" action="#" method = "get">
 
 		<div><h2 id="scorebaord"></h2></div>
-		<h1 class="qh1">Novice Important Programs Quiz</h1>
+		<h1 class="qh1">Novice SSH Quiz</h1>
 		<!--
 			The naming convention for quiz question id's is as follows: Question # Answer #
 			For example Question 2 Answer 4's id would be: "q2a4"
