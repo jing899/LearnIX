880508d19d237509a4f743c9ff6a9bd6581ff7b8
Rachael <rxb9323@rit.edu>
2019-04-24 09:47:03 -0400

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,8 @@ include $path.'assets/inc/header.php';
 <div class="content">
     <div class="empty"></div>
     <h1 id="main_title">Learn<br/>Unix with<br/>LearnIX<br/></h1>
-    <p id="overview">LearnIX is a tutorial website that aims to teach users how to use Unix through quizzes, and interactive activities. LearnIX starts with very simple concepts, teaching users how to navigate the Unix command line and then moves into increasingly more complex ideas for advanced users. A smooth layout allows users to go through tutorials at their own pace and includes a reference page for users to be able to quickly find a command they might need. The site is aimed toward Unix with the purpose of Web Development, and is perfect for anyone from novice to advanced.</p>
+    <p id="overview">LearnIX is a tutorial website that aims to teach users how to use Unix through quizzes, and interactive activities. LearnIX starts with very simple concepts, teaching users how to navigate the Unix command line and then moves into increasingly more complex ideas for advanced users. A smooth layout allows users to go through tutorials at their own pace and includes a reference page for users to be able to quickly find a command they might need. The site is aimed toward Unix with the purpose of Web Development, and is perfect for anyone from novice to advanced. If you have experience
+	with a topic already take the quiz and see how well you really know it!</p>
     <div class="empty2"></div>
     <div class="buttons" id="button_nov">
         <a href="novice/ssh.php"><h2>Novice<br/>Tutorials</h2></a>
